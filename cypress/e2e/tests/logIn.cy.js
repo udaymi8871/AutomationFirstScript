4e13c5e0bd14d03ddfcd6425572f8e2e8e544b1b
apple <apple@apples-MacBook-Pro.local>
2026-01-16 12:08:39 +0530

    removed commented code

--- cypress/e2e/tests/logIn.cy.js
+++ cypress/e2e/tests/logIn.cy.js
@@ -1,43 +1,40 @@
-import user from "../../../cypress/fixtures/user.json"
+import user from "../../../cypress/fixtures/user.json";
 
-describe('Landing Page ', () => {
-
-  it('', () => {
+describe("Landing Page ", () => {
+  it("", () => {
     cy.viewport(1400, 877);
-    cy.visit('https://app.kodnest.in/login', { timeout: 30000 }); // Added timeout for visit
+    cy.visit("https://app.kodnest.in/login", { timeout: 30000 }); // Added timeout for visit
     // Wait for the page to load
-    cy.get('body', { timeout: 10000 }).should('be.visible');
+    cy.get("body", { timeout: 10000 }).should("be.visible");
     // Wait for the login button to be visible
-    cy.get('#login-submit-button', { timeout: 10000 })
-      .should('be.visible')
-      .should('not.be.disabled');
+    cy.get("#login-submit-button", { timeout: 10000 })
+      .should("be.visible")
+      .should("not.be.disabled");
 
     // Wait for loader to disappear
-    cy.get('.loader', { timeout: 10000 }).should('not.exist');
+    cy.get(".loader", { timeout: 10000 }).should("not.exist");
 
     // Wait for the email input to be visible
-    cy.get('#email-input', { timeout: 10000 });
+    cy.get("#email-input", { timeout: 10000 });
 
     // Type email
-    cy.get('#email-input', { timeout: 10000 })
-      .should('be.visible')
+    cy.get("#email-input", { timeout: 10000 })
+      .should("be.visible")
       .click()
-      // .type('uday+practicebatch1-a@kodnest.com', { timeout: 10000 });
+
       .type(user.email, { timeout: 10000 });
 
     // Type password
-    cy.get('#password-input', { timeout: 10000 })
-      .should('be.visible')
-      .should('not.be.disabled')
+    cy.get("#password-input", { timeout: 10000 })
+      .should("be.visible")
+      .should("not.be.disabled")
       .click()
-      // .type('Uday@123', { timeout: 10000 });
+
       .type(user.password, { timeout: 10000 });
 
     // Click login
-    cy.get('#login-submit-button', { timeout: 10000 }).click();
+    cy.get("#login-submit-button", { timeout: 10000 }).click();
 
     // Wait for profile avatar to be visible and click
-
-
   });
 });
